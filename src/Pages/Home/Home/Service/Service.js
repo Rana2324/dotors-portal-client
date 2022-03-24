c8016e407c3dbd0ce38b160346609858c20201ab
Rana2324 <shohelranatonmoy@gmail.com>
2022-03-24 13:20:16 +0900

    complete services part

--- src/Pages/Home/Home/Service/Service.js
+++ src/Pages/Home/Home/Service/Service.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
+
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
+
 import Typography from "@mui/material/Typography";
 
 const Service = (props) => {
@@ -12,7 +12,7 @@ const Service = (props) => {
 
   return (
     <Grid item xs={4} sm={4} md={4}>
-      <Card sx={{ minWidth: 275, border: 0,  boxShadow: 0   }}>
+      <Card sx={{ minWidth: 275, border: 0, boxShadow: 0 }}>
         <CardMedia
           component="img"
           style={{ width: "auto", height: "80px", margin: "0 auto" }}
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Box from "@mui/material/Box";
-
+import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import { Container } from "@mui/material";
 import Service from "../Home/Service/Service";
@@ -33,6 +33,12 @@ const Services = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Container>
+            <Typography sx ={{color: 'success.main', fontWeight: '500', m: 2}} variant="h6" component="div">
+                 OUR SERVICES
+            </Typography>
+            <Typography sx ={{fontWeight: '500', m: 5}} variant="h4" component="div">
+                Services We Provide
+            </Typography>
         <Grid
           container
           spacing={{ xs: 2, md: 3 }}
